0b27b461cbbb6a94e6196b9cafc840cd9daf6ae5
Caleb Campbell <calebcampbellcrm@gmail.com>
2023-01-03 12:41:38 -0700

    wip

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -6,14 +6,26 @@ function Category({category}) {
     let fontColor = 'white'
 
     const [openCategories, setOpenCategories] = useState({
-        Bullet: false,
+        Bullet: true,
         Blitz: false, 
         Rapid: false
     })
 
+    const openCategory = (e) => {
+      if(e.target.id === 'Bullet'){
+        setOpenCategories({...openCategories, Bullet: !openCategories.Bullet })}   
+    }
+
+    const styleObj = {
+    transform: openCategories[category.name] ? 'transform: rotateZ(180deg)' : 'transform: rotateZ(0deg)'
+    }
+
   return (
     <div key={category.points} className='category'>
-        <h2 id={category.name}>{category.name}</h2>
+      <div className='category-header'>
+        <h2 onClick={openCategory} id={category.name}>{category.name} </h2>
+        <p style={styleObj} className='caret'>▲</p>
+      </div>
 
                 <strong key={category.points} className='elotitle'>ELO: {category.points.map(item => {
                     item[3] > previous ? fontColor = 'green' : fontColor = 'red'
