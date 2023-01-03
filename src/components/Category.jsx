27a3892d9b6e55a5b8cf871665d1fe309c4e60cf
Caleb Campbell <calebcampbellcrm@gmail.com>
2023-01-02 18:15:17 -0700

    added gitignore and todolist. stable build

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -4,15 +4,22 @@ import '../styles/category.css'
 function Category({category}) {
     let previous = 0
     let fontColor = 'white'
+
+    const [openCategories, setOpenCategories] = useState({
+        Bullet: false,
+        Blitz: false, 
+        Rapid: false
+    })
+
   return (
-    <div className='category'>
-        <h2>{category.name}</h2>
-        {/* <p>{category.points[1]} - {category.points[2]} - {category.points[0]}</p> */}
-        <strong className='elotitle'>ELO: {category.points.map(item => {
-                item[3] > previous ? fontColor = 'green' : fontColor = 'red'
-                previous = item[3]
-            return (
-        <div className='timebox'>
+    <div key={category.points} className='category'>
+        <h2 id={category.name}>{category.name}</h2>
+
+                <strong key={category.points} className='elotitle'>ELO: {category.points.map(item => {
+                    item[3] > previous ? fontColor = 'green' : fontColor = 'red'
+                    previous = item[3]
+                    return (
+                        <div className='timebox'>
         <p className='date'>{item[1]} - {item[2]}  - {item[0]}</p>
         <p style={{color: fontColor}} className='elo'>{item[3]} {fontColor === 'green' ? '▲' : '▼'}</p>
         </div>
